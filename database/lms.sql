51c295b07f47665b9f0ec7c31f4b74cf773f0028
Kokila Ekanayake <kokilaprabuddha@gmail.com>
2022-05-08 03:05:27 +0530

    Update lms.sql

--- database/lms.sql
+++ database/lms.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 10.4.17-MariaDB : Database - lms
+MySQL - 10.4.24-MariaDB : Database - lms
 *********************************************************************
 */
 
@@ -15,6 +15,29 @@ MySQL - 10.4.17-MariaDB : Database - lms
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`lms` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `lms`;
+
+/*Table structure for table `due_payment` */
+
+DROP TABLE IF EXISTS `due_payment`;
+
+CREATE TABLE `due_payment` (
+  `idDuePayment` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` double DEFAULT NULL,
+  `idStudent` int(11) DEFAULT NULL,
+  `idSubject` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idDuePayment`),
+  KEY `fk_due_payment_id_student_1` (`idStudent`),
+  KEY `fk_due_payment_id_subject_1` (`idSubject`),
+  CONSTRAINT `fk_due_payment_id_student_1` FOREIGN KEY (`idStudent`) REFERENCES `student` (`idstudent`),
+  CONSTRAINT `fk_due_payment_id_subject_1` FOREIGN KEY (`idSubject`) REFERENCES `subject` (`idsubject`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `due_payment` */
+
+insert  into `due_payment`(`idDuePayment`,`amount`,`idStudent`,`idSubject`) values 
+(1,700,2,1),
+(2,700,1,1),
+(3,100,1,2);
 
 /*Table structure for table `exam` */
 
@@ -73,13 +96,52 @@ CREATE TABLE `lecturer` (
   KEY `fk_lecturer_iduser1_idx` (`iduser`),
   CONSTRAINT `fk_lecturer_idsubject1_idx` FOREIGN KEY (`idsubject`) REFERENCES `subject` (`idsubject`) ON UPDATE NO ACTION,
   CONSTRAINT `fk_lecturer_iduser1_idx` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `lecturer` */
 
 insert  into `lecturer`(`idlecturer`,`iduser`,`idsubject`) values 
 (1,1,1),
-(2,4,1);
+(2,4,1),
+(3,5,2);
+
+/*Table structure for table `notification` */
+
+DROP TABLE IF EXISTS `notification`;
+
+CREATE TABLE `notification` (
+  `idnotification` int(11) NOT NULL AUTO_INCREMENT,
+  `notif` text DEFAULT NULL,
+  PRIMARY KEY (`idnotification`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `notification` */
+
+insert  into `notification`(`idnotification`,`notif`) values 
+(1,'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
+
+/*Table structure for table `payroll` */
+
+DROP TABLE IF EXISTS `payroll`;
+
+CREATE TABLE `payroll` (
+  `idPayroll` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` double DEFAULT NULL,
+  `idStudent` int(11) DEFAULT NULL,
+  `idSubject` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPayroll`),
+  KEY `fk_payroll_id_student_1` (`idStudent`),
+  KEY `fk_payroll_id_subject_1` (`idSubject`),
+  CONSTRAINT `fk_payroll_id_student_1` FOREIGN KEY (`idStudent`) REFERENCES `student` (`idstudent`),
+  CONSTRAINT `fk_payroll_id_subject_1` FOREIGN KEY (`idSubject`) REFERENCES `subject` (`idsubject`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `payroll` */
+
+insert  into `payroll`(`idPayroll`,`amount`,`idStudent`,`idSubject`) values 
+(1,200,1,1),
+(2,200,1,1),
+(3,100,1,1);
 
 /*Table structure for table `recording` */
 
@@ -95,10 +157,6 @@ CREATE TABLE `recording` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `recording` */
-
-insert  into `recording`(`idrecording`,`urlstring`,`idsubject`) values 
-(2,'../uploads/recording_20220505184213.mp4',1),
-(3,'../uploads/recording_20220505185258.mp4',1);
 
 /*Table structure for table `scheduled_event` */
 
@@ -155,13 +213,14 @@ CREATE TABLE `student_subjects` (
   KEY `fk_student_subjects_idsubject1_idx` (`idsubject`),
   CONSTRAINT `fk_student_subjects_idstudent1_idx` FOREIGN KEY (`idstudent`) REFERENCES `student` (`idstudent`),
   CONSTRAINT `fk_student_subjects_idsubject1_idx` FOREIGN KEY (`idsubject`) REFERENCES `subject` (`idsubject`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `student_subjects` */
 
 insert  into `student_subjects`(`idstudent_subjects`,`idstudent`,`idsubject`) values 
-(1,1,1),
-(2,2,1);
+(2,2,1),
+(4,1,1),
+(5,1,2);
 
 /*Table structure for table `subject` */
 
@@ -172,12 +231,35 @@ CREATE TABLE `subject` (
   `subjectname` varchar(60) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`idsubject`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `subject` */
 
 insert  into `subject`(`idsubject`,`subjectname`,`status`) values 
-(1,'PHP','active');
+(1,'PHP 2022','active'),
+(2,'HTML 2022','active');
+
+/*Table structure for table `unread_notification` */
+
+DROP TABLE IF EXISTS `unread_notification`;
+
+CREATE TABLE `unread_notification` (
+  `idunreadnotification` int(11) NOT NULL AUTO_INCREMENT,
+  `idnotification` int(11) DEFAULT NULL,
+  `idstudent` int(11) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idunreadnotification`),
+  KEY `fk_unread_notification_notif_1` (`idnotification`),
+  KEY `fk_unread_notification_student_1` (`idstudent`),
+  CONSTRAINT `fk_unread_notification_notif_1` FOREIGN KEY (`idnotification`) REFERENCES `notification` (`idnotification`),
+  CONSTRAINT `fk_unread_notification_student_1` FOREIGN KEY (`idstudent`) REFERENCES `student` (`idstudent`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `unread_notification` */
+
+insert  into `unread_notification`(`idunreadnotification`,`idnotification`,`idstudent`,`status`) values 
+(3,1,2,'active'),
+(4,1,1,'read');
 
 /*Table structure for table `user` */
 
@@ -191,7 +273,7 @@ CREATE TABLE `user` (
   `type` varchar(45) DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
@@ -199,7 +281,8 @@ insert  into `user`(`iduser`,`email`,`password`,`name`,`type`,`status`) values
 (1,'admin@mail.com','123','Super Admin','superadmin','active'),
 (2,'test1@gmail.com','123','test1@gmail.com','student','active'),
 (3,'test2@gmail.com','123','test2@gmail.com','student','active'),
-(4,'kosala@gmail.com','1234','Kosala','lecturer','active');
+(4,'kosala@gmail.com','1234','Kosala','lecturer','active'),
+(5,'janith@gmail.com','123','Janith','lecturer','active');
 
 /*Table structure for table `viewed_event` */
 
